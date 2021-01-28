5f7ba11b6754212417839d8352a3a07419fab904
Corbo2000 <47409337+Corbo2000@users.noreply.github.com>
2021-01-28 15:57:31 -0600

    Added initial files

---
+++ miniProject1_PL1.srcs/sources_1/new/miniProjectSource.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/28/2021 03:56:48 PM
+// Design Name: 
+// Module Name: miniProjectSource
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module miniProjectSource(
+
+    );
+endmodule
